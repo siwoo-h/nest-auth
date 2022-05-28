3905a43cdace5d20d7875368b9c093342a098237
Siwoo Hwang <tldn48@gmail.com>
2022-05-28 23:03:32 +0900

    chore: add typeORMConfig, dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { typeORMConfig } from './config/typeorm.config';
 
 @Module({
-  imports: [AuthModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [TypeOrmModule.forRoot(typeORMConfig), AuthModule],
 })
 export class AppModule {}
