d50baa80381dc399b8a95de8a62c02f2d358a745
Siwoo Hwang <tldn48@gmail.com>
2022-06-01 22:23:22 +0900

    feat: throttler 추가
    @nestjs/throttler

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AuthModule } from '@src/auth/auth.module';
 import { typeORMConfig } from '@src/config/typeorm.config';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeORMConfig), AuthModule],
+  imports: [
+    TypeOrmModule.forRoot(typeORMConfig),
+    ThrottlerModule.forRoot({
+      ttl: 60,
+      limit: 10,
+    }),
+    AuthModule,
+  ],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
