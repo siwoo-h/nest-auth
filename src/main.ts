5da2a3bcf6aa3c3e80bf58f7e420b4ce37393ff6
Siwoo Hwang <tldn48@gmail.com>
2022-06-15 00:00:52 +0900

    chore: @nestjs/swagger 추가

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,17 @@
 import * as config from 'config';
 import { NestFactory } from '@nestjs/core';
 
-import { AppModule } from '@src/app.module';
 import { Logger } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+import { AppModule } from '@src/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder().setTitle('Cats example').setDescription('The cats API description').setVersion('1.0').addTag('cats').build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   const serverConfig = config.get('server');
   const port = serverConfig.port;
