90d6b9df287bd49358f4ec83e06d99c2651b70d3
Siwoo Hwang <tldn48@gmail.com>
2022-06-15 00:03:16 +0900

    fix: naming 수정

--- src/main.ts
+++ src/main.ts
@@ -9,8 +9,8 @@ import { AppModule } from '@src/app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const config = new DocumentBuilder().setTitle('Cats example').setDescription('The cats API description').setVersion('1.0').addTag('cats').build();
-  const document = SwaggerModule.createDocument(app, config);
+  const documentBuilder = new DocumentBuilder().setTitle('Cats example').setDescription('The cats API description').setVersion('1.0').addTag('cats').build();
+  const document = SwaggerModule.createDocument(app, documentBuilder);
   SwaggerModule.setup('api', app, document);
 
   const serverConfig = config.get('server');
